2d5951b4395e55aa210f3ccacd25d2149059909d
Sundar Tamang <sundartamang6@gmail.com>
2025-09-15 21:01:35 +0545

    feat: horizental bar chart

--- src/app/components/upload-month-wise/upload-month-wise.component.ts
+++ src/app/components/upload-month-wise/upload-month-wise.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { AgCharts } from 'ag-charts-angular';
+import {
+  AgBarSeriesOptions,
+  AgCategoryAxisOptions,
+  AgChartCaptionOptions,
+  AgChartLegendOptions,
+  AgChartOptions,
+  AgChartSubtitleOptions,
+  AgLineSeriesOptions,
+  AgNumberAxisOptions,
+} from 'ag-charts-community';
 
 @Component({
   selector: 'app-upload-month-wise',
   standalone: true,
-  imports: [],
+  imports: [AgCharts],
   templateUrl: './upload-month-wise.component.html',
-  styleUrl: './upload-month-wise.component.scss'
+  styleUrl: './upload-month-wise.component.scss',
 })
 export class UploadMonthWiseComponent {
+  public chartOptions: any;
 
+  constructor() {
+    this.chartOptions = {
+      title: { text: 'NO. OF UPLOAD USERWISE' },
+      data: [
+        { username: 'Aayush', uploads: 118 },
+        { username: 'Aman', uploads: 125 },
+        { username: 'Cathy', uploads: 102 },
+        { username: 'Danny', uploads: 122 },
+        { username: 'Ethan', uploads: 116 },
+        { username: 'Evan', uploads: 126 },
+        { username: 'Fey', uploads: 105 },
+        { username: 'Gia', uploads: 124 },
+      ] as any[],
+      series: [
+        {
+          type: 'bar',
+          xKey: 'username',
+          yKey: 'uploads',
+          fill: '#03A9F4',
+          direction: 'horizontal',
+          highlightStyle: {
+            item: {
+              fill: 'transparent',
+              stroke: 'transparent',
+            },
+          },
+        } as AgBarSeriesOptions,
+      ],
+      axes: [
+        { type: 'category', position: 'left', title: { text: 'Username' } },
+        { type: 'number', position: 'bottom', title: { text: '' } },
+      ] as any[],
+      legend: { enabled: false },
+    };
+  }
 }
